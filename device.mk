d7766a4cce90679b1ab7cc588a5c13b791094bc7
Rick M <spookcity138@gmail.com>
2019-04-04 23:04:06 +0200

    add sswap binary

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/sbin/cbd:root/sbin/cbd
 
+# SSWAP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/sbin/sswap:root/sbin/sswap
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
