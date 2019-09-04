782573b4b13d6d5240f058ffff13815c2772b31a
Rick M <spookcity138@gmail.com>
2019-09-04 16:40:01 +0200

    Add priv-app permissions
    Missing these permissions was causing our device not to boot due to this
    commit:
    
    https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/251804

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml  \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml::$(TARGET_COPY_OUT_VENDOR)/etc//media_profiles_V1_0.xml
 
+# Priv-app Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-kminilte.xml:system/etc/permissions/privapp-permissions-kminilte.xml
+
 # NFC
 #PRODUCT_PACKAGES += \
   #  com.android.nfc_extras \
