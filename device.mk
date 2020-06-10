4b8d6b4398d38b6d82e009c03414851ed51f2523
Rick M <spookcity138@gmail.com>
2020-06-10 14:44:29 +0200

    kminite: Add SimpleCamera to build
    All credit:
    https://f-droid.org/en/packages/com.simplemobiletools.camera/

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += \
     AriesParts \
     DockAudio
 
+# Camera
+PRODUCT_PACKAGES += \
+	SimpleCamera
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=true
 
