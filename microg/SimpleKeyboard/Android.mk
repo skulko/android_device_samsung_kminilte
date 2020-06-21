474032e7eab005b0046cc8c5a7da2bf992a12da4
skulko <23030592+skulko@users.noreply.github.com>
2020-06-21 12:06:22 +0200

    Update SimpleKeyboard

--- microg/SimpleKeyboard/Android.mk
+++ microg/SimpleKeyboard/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := SimpleKeyboard
-LOCAL_SRC_FILES := rkr.simplekeyboard.inputmethod_68.apk
+LOCAL_SRC_FILES := rkr.simplekeyboard.inputmethod_71.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
