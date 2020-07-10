710d93f50876e6b16c013e0f86d8f821f471ea79
skulko <23030592+skulko@users.noreply.github.com>
2020-07-10 11:35:55 +0200

    Update OpenLauncher

--- microg/OpenLauncher/Android.mk
+++ microg/OpenLauncher/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := OpenLauncher
-LOCAL_SRC_FILES := com.benny.openlauncher_40.apk
+LOCAL_SRC_FILES := com.benny.openlauncher_41.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
