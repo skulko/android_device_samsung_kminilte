af4560edbb543d187dc5abc6fa0f303067e03940
skulko <23030592+skulko@users.noreply.github.com>
2020-09-06 10:36:18 +0200

    Replace old GSM Location app with (self-compiled) ploink's fork (1.4.25-ploink)

--- microg/GsmLocation/Android.mk
+++ microg/GsmLocation/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := GsmLocation
-LOCAL_SRC_FILES := org.fitchfamily.android.gsmlocation_68.apk
+LOCAL_SRC_FILES := org.fitchfamily.android.gsmlocation.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
